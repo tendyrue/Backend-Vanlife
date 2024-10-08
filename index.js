3af3394f092c2bffa3698854c7ad98dcc192d08f
tendyrue <mutsamwirarutendo@gmail.com>
2024-10-08 03:25:09 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-const express = require('express')
-// Using Node.js `require()`
+const express = require('express');
 const mongoose = require('mongoose');
-const app = express()
-
-app.get('/', (req, res) => {
-    res.send('Hello from Node API Server Updated');
-});
+const Booking = require('./models/booking.model');
+const app = express();
 
 
-app.post(
-    '/api/bookings', (req, res) =>{
-        console.log(req.body);
-        res.send("Data Received");
+
+app.use(express.json());
+
+
+mongoose.connect('mongodb+srv://mutsamwirarutendo:5qz1VXVrm1ZWrlsi@backenddb.wheqf.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB')
+  .then(() => {
+    console.log('Connected to the database!');
+
+    // Define routes only after successful DB connection
+    app.get('/', (req, res) => {
+      res.send('Hello from Node API');
     });
 
 
 
-mongoose.connect('mongodb+srv://wisdombalicholo:I9EBIfKa5reqmgJB@vanlifedb.ltvwc.mongodb.net/Node-API?retryWrites=true&w=majority&appName=VanLifeDB')
-  .then(() => {
-    console.log('Connected!');
-    app.listen(3000, () =>{
-        console.log('server is running on port 3000');
-    })
-})
-.catch(() => {
-    console.log('connection Failed');
-});
+    app.post('/api/bookings', async (req, res) => {
+     try {
+
+        const booking = await Booking.create(req.body);
+        res.status(200).json(booking);
+     }catch (erroe) {
+        res.status(500).json({message: error.message});
+     }
+     
+    });
+    // Start server only after successful DB connection
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+  })
+  .catch((err) => {
+    console.error('Connection Failed:', err);
+  });
